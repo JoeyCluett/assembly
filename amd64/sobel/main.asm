1038ba16958819b6184c060f194adaf523a63274
JoeyCluett <joesunglassescool@gmail.com>
2019-11-07 17:55:29 -0600

    continued on sobel operator

--- amd64/sobel/main.asm
+++ amd64/sobel/main.asm
@@ -1,7 +1,7 @@
 ;
-; an assembly program that applies the Sobel operator to an 
-; image array that is already in memory. it needs to know 
-; the height and width of the image and needs a src array 
+; an assembly program that applies the Sobel operator to an
+; image array that is already in memory. it needs to know
+; the height and width of the image and needs a src array
 ; to read from and a dest array to write results to
 ;
 ; the sobel operator is a 3x3 image kernel:
@@ -9,6 +9,9 @@
 ;  -1  -2  -1
 ;   0   0   0
 ;   1   2   1
+;
+; this program does not pad anything. it simply ignores the
+; outermost rows and columns of the input image
 ;
 
 extern printf
@@ -29,9 +32,9 @@ section .data
 section .text
 apply_sobel:
     ; create stack frame
-    push rbp
+    push rbp        ; save the previous value of rbp
     mov rbp, rsp    ; rbp now points at its own previous value this creates a 'stack trace'
-    sub rsp, 80    ; allocate 80 bytes for local storage
+    sub rsp, 72     ; allocate 80 bytes for local storage
 
     ;
     ; arguments:
@@ -41,32 +44,29 @@ apply_sobel:
     ; rcx : image width
     ;
 
+    ; print the address of the src image
+    push rdi
+    ;mov rdi, rdi
+    call print_pointer_subroutine
+    pop rdi
+
+    ; print the address of the dest image
+    push rsi
+    mov rdi, rsi
+    call print_pointer_subroutine
+    pop rsi
+
     ; do nothing if either of the sizes is less than 3
     cmp rdx, 3
     jl clean_and_return
     cmp rcx, 3
     jl clean_and_return
 
-    mov [rsp+8],  rdi
-    mov [rsp+16], rsi
-    mov [rsp+24], rdx
-    mov [rsp+32], rcx
-
-    mov rdi, image_size_fmt
-    mov rsi, [rsp+24]
-    mov rdx, [rsp+32]
-    call printf
-
-    mov rdi, [rsp+8]
-    mov rsi, [rsp+16]
-    mov rdx, [rsp+24]
-    mov rcx, [rsp+32]
-
     ; now the actual calculation can begin
     ; first subtract 2 from each dimension
-    sub rdx, 2  ; img h
-    sub rcx, 2  ; img w
-    shl rcx, 3      ; rcx will be treated like a raw byte offset
+    sub rdx, 2 ; img h
+    sub rcx, 2 ; img w
+    shl rcx, 3 ; rcx is now the width of each row in bytes
 
     ;jmp clean_and_return
 
@@ -87,16 +87,20 @@ apply_sobel:
 
     subsd xmm0, xmm0    ; need a zero to start adding to
 
-    mov r10, r8     ; r8 is the current vertical index
-    imul r10, rcx   ; generate the byte offset for this row
-    add r10, r9     ; r9 is the byte offset of the current element
-    add r10, rdi    ; add the base pointer before fetching
+    mov  r10, r8   ; r8 is the current vertical index
+    imul r10, rcx  ; generate the byte offset for this row
+    add  r10, r9   ; r9 is the byte offset of the current element
+    add  r10, rdi  ; add the base pointer before fetching
 
     movsd xmm1, QWORD [sobel] ; fetch upper left sobel coeffient
     mulsd xmm1, QWORD [r10]   ; fetch the current element
     addsd xmm0, xmm1    ; add to the cumulative sum for this window
 
     ; look at this awesome clusterf*ck of RAW and WAW hazards!!
+
+    movsd xmm1, QWORD [sobel + 0]
+    mulsd xmm1, QWORD [r10 + 0]
+    addsd xmm0, xmm1
 
     movsd xmm1, QWORD [sobel + 8]
     mulsd xmm1, QWORD [r10 + 8]
@@ -106,18 +110,37 @@ apply_sobel:
     mulsd xmm1, QWORD [r10 + 16]
     addsd xmm0, xmm1
 
+    add r10, rcx  ; jump to the next row in the src array
+
     movsd xmm1, QWORD [sobel + 24]
-    mulsd xmm1, QWORD [r10 + 24]
+    mulsd xmm1, QWORD [r10 + 0]
     addsd xmm0, xmm1
 
-    ; ....................................................
-    ; we aren't done evaluating the Sobel operator but 
-    ; i want to get some intermediate results rn
-    ; ....................................................
+    movsd xmm1, QWORD [sobel + 32]
+    mulsd xmm1, QWORD [r10 + 8]
+    addsd xmm0, xmm1
+
+    movsd xmm1, QWORD [sobel + 40]
+    mulsd xmm1, QWORD [r10 + 16]
+    addsd xmm0, xmm1
+
+    add r10, rcx   ; jump to the next row in the src array
+
+    movsd xmm1, QWORD [sobel + 48]
+    mulsd xmm1, QWORD [r10 + 0]
+    addsd xmm0, xmm1
+
+    movsd xmm1, QWORD [sobel + 56]
+    mulsd xmm1, QWORD [r10 + 8]
+    addsd xmm0, xmm1
+
+    movsd xmm1, QWORD [sobel + 64]
+    mulsd xmm1, QWORD [r10 + 16]
+    addsd xmm0, xmm1
 
     ; we can calculate the destination address using r10 as its no longer needed at this point
-    mov r10, ...
-    mov r11, ...
+    ;mov r10, ...
+    ;mov r11, ...
 
     ; condition for next x
     ;inc r9
@@ -132,7 +155,7 @@ apply_sobel:
 
   clean_and_return:
     ; destroy stack frame
-    add rsp, 80 ; deallocate the stack space we requested
+    add rsp, 72 ; deallocate the stack space we requested
     pop rbp     ; remove this part of the stack trace
     ret         ; jump back to caller
 
@@ -140,14 +163,40 @@ apply_sobel:
   invalid_image_size:
     jmp clean_and_return ; jump to regular cleanup routine
 
+  print_pointer_subroutine:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 80
 
+    mov [rsp+8],  rdi
+    mov [rsp+16], rsi
+    mov [rsp+24], rdx
+    mov [rsp+32], rcx
+    mov [rsp+40], r8
+    mov [rsp+48], r9
+    mov [rsp+56], r10
+    mov [rsp+64], r11
 
+    mov rdi, print_pointer
+    mov rsi, [rsp+8]
+    call printf
 
+    mov rdi, [rsp+8]
+    mov rsi, [rsp+16]
+    mov rdx, [rsp+24]
+    mov rcx, [rsp+32]
+    mov r8,  [rsp+40]
+    mov r9,  [rsp+48]
+    mov r10, [rsp+56]
+    mov r11, [rsp+64]
 
+    add rsp, 80
+    pop rbp
+    ret
 
 ;
 ; ... you might be an assembly programmer
 ;
 ; if you use DWORD to save 4 bytes in your executable
 ; if you know what a self-referential pointer is and regularly use it
-; 
+;
--- amd64/sobel/main.c
+++ amd64/sobel/main.c
@@ -3,9 +3,9 @@
 void apply_sobel(double* src, double* dest, int h, int w);
 
 double src[16] = {
-    1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0,
+    4.0, 4.0, 4.0, 4.0,
+    3.0, 3.0, 3.0, 3.0,
+    2.0, 2.0, 2.0, 2.0,
     1.0, 1.0, 1.0, 1.0
 };
 
@@ -16,7 +16,10 @@ int main(int argc, char* argv[]) {
     printf("Addr of array: %p\n", (void*)src);
     apply_sobel(src, dest, 4, 4);
 
-    
+    int j;
+    for(j = 0; j < 16; j += 4) {
+        printf("%lf %lf %lf %lf\n", dest[j+0], dest[j+1], dest[j+2], dest[j+3]);
+    }
 
     return 0;
 }
