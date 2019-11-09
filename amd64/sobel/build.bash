ea9a06785312cf343ab24a70c17ac98d28306b0a
JoeyCluett <joesunglassescool@gmail.com>
2019-11-08 19:42:18 -0600

    use CPUID to determine if the SIMD routine can be run. avoids SIGILL exception

--- amd64/sobel/build.bash
+++ amd64/sobel/build.bash
@@ -5,6 +5,5 @@ yasm -f elf64 -l sobel_gp.lst   sobel_gp/main.asm   -o sobel_gp.o
 yasm -f elf64 -l sobel_simd.lst sobel_simd/main.asm -o sobel_simd.o
 yasm -f elf64 -l sobel.lst      main.asm           -o sobel.o
 
-gcc -c -o main.o main.c 
+gcc -c -o main.o main.c
 gcc -o main main.o sobel_gp.o sobel_simd.o sobel.o
-
--- amd64/sobel/main.asm
+++ amd64/sobel/main.asm
@@ -9,7 +9,8 @@ extern apply_simd_sobel ; a 4x SIMD routine. requries support for certain avx ex
 global sobel
 
 section .data
-    jump_lut: dq apply_gp_sobel, apply_simd_sobel
+    ;sobel_jump_lut: dq apply_gp_sobel, apply_simd_sobel
+    sobel_jump_lut: dq apply_gp_sobel, test_apply_simd
 
     normal_ret_val:  dq 0x00000000
     simd_no_support: dq 0x00000001
@@ -32,11 +33,44 @@ sobel:
     cmp r8, 2
     jge end_sobel
     shl r8, 3
-    call [r8 + jump_lut]
-    ;add r8, jump_lut
-    ;mov r8, [r8]
-    ;call r8
+    call [r8 + sobel_jump_lut]
 
   end_sobel:
+    pop rbp
+    ret
+
+  ; when trying to use the SIMD routine, make sure the CPU suports it first
+  test_apply_simd:
+
+    ; stack frame
+    push rbp
+    mov rbp, rsp
+
+    push rdx  ; save these as they will be overwritten by cpuid
+    push rcx  ; ...
+
+    mov eax, 7  ; prep cpuid to get extended cpu features
+    mov ecx, 0  ; ...
+    cpuid
+
+    pop rcx ; restore the overwritten regisers that we saved
+    pop rdx ; ...
+
+    ; cpu information will overwrite ebx, ecx, edx
+    ; avx2 flag is ebx[5]
+    and ebx, DWORD 0x0010
+    cmp ebx, 0x0010
+    jne use_gp_sobel
+
+    ; the avx2 bit is true, use the optimized version
+    call apply_simd_sobel
+
+    pop rbp
+    ret
+
+  use_gp_sobel:
+    ; the avx2 bit is false, use the gp vesion
+    call apply_gp_sobel
+
     pop rbp
     ret
